e33468e349f45234b021ad1ca78611440f9abe7e
Dipankar Upadhyaya <=>
2023-09-22 19:13:21 +0530

    update: styling updated

--- Footballers/models.py
+++ Footballers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Footballer:
+    Name: str
+    Position: str
+    Team: str
+
+    def __init__(self, Name, Position, Team):
+        self.Name = Name
+        self.Position = Position
+        self.Team = Team
+
